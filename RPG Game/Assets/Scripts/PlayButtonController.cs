25e02449c2312dfcfe7207f1f428f5435390acd3
SeventyFox92475 <ethan.culp101@gmail.com>
2020-02-12 13:30:13 -0700

    Changes to update game (Added a semi UI)

---
+++ RPG Game/Assets/Scripts/PlayButtonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButtonController : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite nextSprite;
+    public Sprite prevSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Play Button" && Input.GetMouseButton(0))
+            {
+                spriteRenderer.sprite = nextSprite;
+            } else
+            {
+                spriteRenderer.sprite = prevSprite;
+            }
+        }
+    }
+}
